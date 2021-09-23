abee05778499ad157dc9de00dc00ed135a7424e1
pororili <xiozioll@gmail.com>
2021-09-23 21:59:12 +0900

    Init

---
+++ src/main/java/com/maplefactory/mapleshop/MapleshopApplication.java
@@ -0,0 +1,13 @@
+package com.maplefactory.mapleshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapleshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapleshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maplefactory/mapleshop/MapleshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maplefactory.mapleshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapleshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
